8e6af7db9b0fdcda4208361165781a4b813fc463
ole kristian homelien <ole_kristian92@hotmail.com>
2020-10-14 22:36:23 +0200

    Changes

---
+++ VariantMeshEditor/Views/EditorViews/AnimationViews/AnimationFragmentExplorerView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace VariantMeshEditor.Views.EditorViews.AnimationViews
+{
+    /// <summary>
+    /// Interaction logic for AnimationFragmentExplorer.xaml
+    /// </summary>
+    public partial class AnimationFragmentExplorerView : UserControl
+    {
+        public AnimationFragmentExplorerView()
+        {
+
+
+            MyItemsSource = new ObservableCollection<Customer>();
+            MyItemsSource.Add(new Customer() { Name = "Dogman" });
+            MyItemsSource.Add(new Customer() { Name = "Dogman2" });
+            MyItemsSource.Add(new Customer() { Name = "train" });
+            MyItemsSource.Add(new Customer() { Name = "line" });
+            MyItemsSource.Add(new Customer() { Name = "Jenny" });
+            InitializeComponent();
+    
+        }
+
+
+
+        public ObservableCollection<Customer> MyItemsSource { get; set; }
+        public Customer MySelectedItem { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
