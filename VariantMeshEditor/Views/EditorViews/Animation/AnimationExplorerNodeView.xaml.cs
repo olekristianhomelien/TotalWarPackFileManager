6345e3ae620d770a712bbeee6b5cfec499268abe
ole kristian homelien <ole_kristian92@hotmail.com>
2020-11-12 10:52:30 +0100

    Changes - animation view mostly working

---
+++ VariantMeshEditor/Views/EditorViews/Animation/AnimationExplorerNodeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace VariantMeshEditor.Views.EditorViews.Animation
+{
+    /// <summary>
+    /// Interaction logic for AnimationExplorerItemView.xaml
+    /// </summary>
+    public partial class AnimationExplorerNodeView : UserControl
+    {
+        public AnimationExplorerNodeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
